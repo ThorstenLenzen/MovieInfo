8ce9728eb5e55e2f45a79840efc2a0625838093d
ThorstenLenzen <thorstenlenzen@outlook.com>
2016-11-14 17:53:26 +0100

    MovieRouter finished.

--- Server.Node.Express/app/routes/movieRouter.ts
+++ Server.Node.Express/app/routes/movieRouter.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
 import { Movie } from '../dataaccess';
+import { IMovie } from '../model';
 
 export class MovieRouter {
 
@@ -11,35 +12,67 @@ export class MovieRouter {
         this.router.get('/', this.getMovies);
         this.router.get('/:id', this.getMovieById);
         this.router.post('/', this.createMovie);
+        this.router.put('/:id', this.updateMovie);
+        this.router.patch('/:id', this.updateMovie);
+        this.router.delete('/:id', this.deleteMovie);
     }
 
-    //movie/
-    public getMovies(request: Request, response: Response): void {
+    // GET /movie
+    private getMovies(request: Request, response: Response): void {
         Movie.find({ }, (error, movies) => {
             if (error) {
-                response.status(400);
+                response.status(500).json(error);
             } else {
                 response.status(200).json(movies);
             }
         });
     }
 
-    //movie/5804c380c28a3bc7eaeab26a
-    public getMovieById(request: Request, response: Response): void {
+    // GET /movie/5804c380c28a3bc7eaeab26a
+    private getMovieById(request: Request, response: Response): void {
         Movie.findById(request.params['id'], (error, movies) => {
             if (error) {
-                response.status(400);
+                response.status(500).json(error);
             } else {
                 response.status(200).json(movies);
             }
         });
     }
 
-    public createMovie(request: Request, response: Response): void {
-        // let movie = new 
-        // movieRepository.create(movie, (error, movies) => {
+    // POST /movie
+    private createMovie(request: Request, response: Response): void {
+        let movie = new Movie(request.body);
 
-        // });
+        movie.save((error, movie) => {
+            if (error) {
+                response.status(500).json(error);
+            } else {
+                response.status(200).json(movie);
+            }
+        });
+
         response.status(200);
+    }
+
+    // PUT /movie/5804c380c28a3bc7eaeab26a
+    // PATCH /movie/5804c380c28a3bc7eaeab26a
+    private updateMovie(request: Request, response: Response): void {
+        Movie.findOneAndUpdate({_id: request.params['id']}, request.body, (error, movie) => {
+            if (error) {
+                response.status(500).json(error);
+            } else {
+                response.status(200).json(movie);
+            }
+        });
+    }
+
+    private deleteMovie(request: Request, response: Response): void {
+        Movie.remove({_id: request.params['id']}, error => {
+            if (error) {
+                response.status(500).json(error);
+            } else {
+                response.status(200);
+            }
+        });
     }
 }
--- Server.Node.Express/app/webapi.ts
+++ Server.Node.Express/app/webapi.ts
@@ -1,4 +1,5 @@
 import { Application } from 'express';
+import * as bodyParser from 'body-parser';
 import { TestRouter, MovieRouter} from './routes';
 import { RequestLogger } from './middleware';
 
@@ -17,13 +18,13 @@ export class WebApi {
      */
     private configureMiddleware(app: Application) {
         app.use(new RequestLogger().handler);
+        app.use(bodyParser.json());
     }
 
     private configureRoutes(app: Application) {
         app.use('/test', new TestRouter().router);
         app.use('/movie', new MovieRouter().router);
-        // mount more routers here
-        // e.g. app.use("/organisation", organisationRouter);
+        // mount more routers heretask
     }
 
     public run() {
