8568a2715a9a960a8704f88933cedd186f055068
Toto <thorsten.lenzen@outlook.com>
2017-01-03 14:53:44 +0100

    Mongo DB insert script added.

---
+++ Server.NET.Core/Toto.MovieInfo/src/Database.Mongo/Populate-Mongo-Db.js
@@ -0,0 +1,26 @@
+db.movies.insert([
+	{ 
+		"_id" : ObjectId("5063114bd386d8fadbd6b004"), 
+		"Name" : "Star Wars", 
+		"Description" : "Luke Skywalker, a young farmer from the desert planet of Tattooine, must save Princess Leia from the evil Darth Vader.",
+		"ReleaseDate" : "1977"
+	},
+	{ 
+		"_id" : ObjectId("5063114bd386d8fadbd6b005"), 
+		"Name" : "Sabrina", 
+		"Description" : "An ugly duckling having undergone a remarkable change, still harbors feelings for her crush: a carefree playboy, but not before his business-focused brother has something to say about it.",
+		"ReleaseDate" : "1995"
+	},
+	{ 
+		"_id" : ObjectId("5063114bd386d8fadbd6b008"), 
+		"Name" : "Patriot Games", 
+		"Description" : "When CIA Analyst Jack Ryan interferes with an IRA assassination, a renegade faction targets him and his family for revenge.",
+		"ReleaseDate" : "1992"
+	},
+	{ 
+		"_id" : ObjectId("5063114bd386d8fadbd6b009"), 
+		"Name" : "Blade Runner", 
+		"Description" : "A blade runner must pursue and try to terminate four replicants who stole a ship in space and have returned to Earth to find their creator.",
+		"ReleaseDate" : "1982"
+	}]
+)
